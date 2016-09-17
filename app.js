463be4abf4e9bded2b1f885fd311b30b517791ce
Anthony <nick2000003@gmail.com>
2016-09-17 20:22:11 +0000

    website changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
 var nodemailer = require("nodemailer");
-var saves = [{sender: "test@gmail.com", message: "TestMessage"}];
+var saves = [{sender: "test@gmail.com", message: "TestMessage"}, {sender: "LivyJade2003@gmail.com", message: "Wow Anthony, this is great!"}];
 
 function check(spamMessage) {
    if(spamMessage != "" || spamMessage != undefined) {
@@ -30,29 +30,7 @@ app.get("/", function(req, res) {
 app.post("/contact", function(req, res) {
    console.log("Received");
    if(check(req.body.spam) && req.body.email != " " && req.body.message != " ") {
-        //  var smtpTransport = nodemailer.createTransport("SMTP",{
-        //      service: "Gmail",
-        //      auth: {
-        //          user: "BigBear3555@gmail.com",
-        //          pass: "stuff123"
-//              }
-//          });
-//   var mailOptions={
-//         to : to,
-//         subject : req.body.email,
-//         text : req.body.message
-//     }
-//     console.log(mailOptions);
-//     smtpTransport.sendMail(mailOptions, function(error, response){
-//      if(error){
-//             console.log(error);
-//         res.send(error);
-//      }else{
-//             console.log("Message sent: " + response.message);
-//             res.redirect("/");
-//         res.end("sent");
-//          }
-//     });
+
 
     saves.push({sender: req.body.email, message: req.body.message});
     console.log("Saved!");
